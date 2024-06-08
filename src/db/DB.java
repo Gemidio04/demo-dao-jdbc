e7503ad11322f76ba4d10a35ca29b9e6018c784c
Gustavo <gemidio2004@gmail.com>
2024-06-08 13:11:38 -0300

    Department entity class

--- src/db/DB.java
+++ src/db/DB.java
@@ -8,6 +8,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DB {
 
